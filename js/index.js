f7acfed3beee224db02eb27340215e46af02db4c
AndyHuizenga <andy.huizenga@student.ehb.be>
2021-08-14 10:43:45 +0200

    api-fetch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 "use strict";
+class Article {
+    constructor(id, title, content, imageURL, likes, datum) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imageURL = imageURL;
+        this.likes = likes;
+        this.datum = datum;
+    }
+}
+
+let articlesArray = [];
+
+async function data() {
+    await fetch("https://thecrew.cc/news/read.php")
+        .then((response) => response.json())
+        .then((data) => {
+            data.news.forEach((article) => {
+                articlesArray.push(
+                    new Article(
+                        article.UUID,
+                        article.title,
+                        article.content,
+                        article.imageURL,
+                        article.likes,
+                        article.datum
+                    )
+                );
+            });
+            console.log(articlesArray);
+        });
+}
+
+data();
